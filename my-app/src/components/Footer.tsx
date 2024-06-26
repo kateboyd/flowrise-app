2c90d3e08863fe95926937b90945c52555db9c01
Kate <kate.wileman@hotmail.co.uk>
2024-06-26 15:37:52 +0100

    styled header and footer

--- my-app/src/components/Footer.tsx
+++ my-app/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { createClient } from "@/prismicio"
 import { PrismicNextLink } from "@prismicio/next"
 import Link from "next/link"
+import Logo from "./Logo"
+import Bounded from "./Bounded"
 
 export default async function Footer() {
   const client = createClient()
@@ -8,20 +10,24 @@ export default async function Footer() {
   const settings = await client.getSingle("settings")
 
   return (
-    <footer>
-      <Link href="/">{settings.data.site_title}</Link>
-
-      <p>©{new Date().getFullYear()}</p>
-
-      <nav>
-        <ul>
-          {settings.data.navigation.map(({ link, label }) => (
-            <li key={label}>
-              <PrismicNextLink field={link}>{label}</PrismicNextLink>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </footer>
+    <Bounded as="footer">
+      <div className="flex sm:flex-row flex-col justify-between items-center gap-6">
+        <Link href="/">
+          <Logo />
+        </Link>
+        <p className="text-sm">©{new Date().getFullYear()}</p>
+        <nav>
+          <ul className="flex">
+            {settings.data.navigation.map(({ link, label }) => (
+              <li key={label}>
+                <PrismicNextLink field={link} className="p-3">
+                  {label}
+                </PrismicNextLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </Bounded>
   )
 }
